accbbdf574aaf0ffc34880c93e160175087c59f0
romieoetega000 <romieoetega@gmail.com>
2026-07-23 14:58:42 +0000

    Task2 complete

---
+++ Task2.java
@@ -0,0 +1,12 @@
+public class Task2 {
+    public static void main(String[] args) {
+        byte a = 3;
+        char b = '1';
+        short c = 10;
+        int d = 0;
+        float e = 2.0f;
+        boolean f = true;
+        String output = "H" + a + b + c + " w" + d + "rld " + e + " " + f; 
+        System.out.println(output);
+    }
+}
